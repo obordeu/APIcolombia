724c3bc6382c8a4de26a3a75e51ffced83ec8e13
Olivia Bordeu Gazmuri <ogazmuri@princeton.edu>
2017-11-19 14:43:04 -0500

    add ben's code 3

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ def menuMaker():
 	region='ANTIOQUIA'
 	area='BELLAS ARTES'
 	percentile=14
-	wb=loadData()
 	results="HEY"
 	return results
 
 
-def loadData():
-	wb = load_workbook('Options_Outcomes.xlsx')
-	ws = wb.get_sheet_by_name('Sheet1')
-	return ws
+
 
 
 if __name__ == '__main__':
